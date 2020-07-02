e0aa7f68ffc19924c58d02abaabbf7e32ce64bee
luckzhangfengbo <1819067326@qq.com>
2020-07-02 09:58:42 +0800

    1.c

---
+++ 1.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 1.c
+	> Author: zhangfengbo
+	> Mail:1819067326@qq.com 
+	> Created Time: 2020年07月02日 星期四 09时57分42秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Hallo haizei");
+    return 0;
+}
